d513afcba1ea6f30ef82c4fb4861e06ba1a155b7
Miguel Barreto <miguelandres@users.noreply.github.com>
2022-10-25 22:07:24 -0400

    move test to mac/basics.yaml, fix install_workflows

--- mac/install_workflows.sh
+++ mac/install_workflows.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-# Notice this is meant to run from dotfiles_v2/, not from dotfiles_v2/mac
-cd mac/Workflows/Services || exit 1
+cd Workflows/Services || exit 1
 
 for workflow in */
 do
 	workflow=${workflow%/}
 	full_path=$(pwd)/"$workflow"
-	ln -s -F -f "$full_path" "$HOME/Library/Services/$full_path"
+	ln -s -F -f "$full_path" "$HOME/Library/Services/$workflow"
 done
